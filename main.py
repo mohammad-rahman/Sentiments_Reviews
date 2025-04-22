10f5fbce1bb6766dd9567099f3fd10bc43dff0b5
Mohammad Rahman <mohammadrahman332@gmail.com>
2025-04-21 23:35:51 -0400

    Added Code to to main.py

--- main.py
+++ main.py
@@ -9,19 +9,21 @@ def run(filepath: str):
     INSERT DOCSTRING HERE
     """
     # open the json object
-    ...
+    with open(filepath, 'r') as file:
+        file_data=json.load(file)
+
 
     # extract the reviews from the json file
-    ...
+    reviews=file_data.get("results", [])
 
     # get a list of sentiments for each line using get_sentiment
-    ...
+    list = get_sentiment(reviews)
 
     # plot a visualization expressing sentiment ratio
-    ...
+    make_plot(list)
 
     # return sentiments
-    ...
+    return list
 
 
 if __name__ == "__main__":
